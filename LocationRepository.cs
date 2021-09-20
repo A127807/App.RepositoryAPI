6635139cdca63ca24538e3f748c8773beda044b3
a127807 <pete_fittante@progressive.com>
2021-09-20 08:35:53 -0400

    Initil Commit

---
+++ LocationRepository.cs
@@ -0,0 +1,26 @@
+﻿using App.Repository.ApiClient;
+using MilSiteCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repository
+{
+	public class LocationRepository
+	{
+		private readonly IWebApiExecuter _webApiExectuter;
+
+		public LocationRepository(IWebApiExecuter webApiExectuter)
+		{
+			this._webApiExectuter = webApiExectuter;
+		}
+
+		public async Task<IEnumerable<Location>> GetAsync()
+		{
+			return await _webApiExectuter.InvokeGet<IEnumerable<Location>>("api/locations");
+		}
+
+	}
+}
