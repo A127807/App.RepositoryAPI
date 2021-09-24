e2b91e5fe9f31a664d0023ab52c8e1f9ec3bb9d2
a127807 <pete_fittante@progressive.com>
2021-09-24 08:01:52 -0400

    Fix program errors

--- ApiClient/WebApiExecuter.cs
+++ ApiClient/WebApiExecuter.cs
@@ -8,48 +8,49 @@ using System.Threading.Tasks;
 
 namespace App.Repository.ApiClient
 {
-	public class WebApiExecuter : IWebApiExecuter
-	{
-		private readonly HttpClient _httpClient;
-		private readonly string _baseUrl;
+    public class WebApiExecuter : IWebApiExecuter
+    {
+        private readonly string baseUrl;
+        private readonly HttpClient httpClient;
 
-		public WebApiExecuter(string baseUrl, HttpClient httpClient)
-		{
-			this._httpClient = httpClient;
-			this._baseUrl = baseUrl;
+        public WebApiExecuter(string baseUrl,
+            HttpClient httpClient)
+        {
+            this.baseUrl = baseUrl;
+            this.httpClient = httpClient;
 
-			httpClient.DefaultRequestHeaders.Accept.Clear();
-			httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-		}
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
 
-		public async Task<T> InvokeGet<T>(string uri)
-		{
-			return await _httpClient.GetFromJsonAsync<T>(GetUrl(uri));
-		}
+        public async Task<T> InvokeGet<T>(string uri)
+        {
+            return await httpClient.GetFromJsonAsync<T>(GetUrl(uri));
+        }
 
-		public async Task<T> InvokePost<T>(string uri, T obj)
-		{
-			var response = await _httpClient.PostAsJsonAsync(GetUrl(uri), obj);
-			response.EnsureSuccessStatusCode();
+        public async Task<T> InvokePost<T>(string uri, T obj)
+        {
+            var response = await httpClient.PostAsJsonAsync(GetUrl(uri), obj);
+            response.EnsureSuccessStatusCode();
 
-			return await response.Content.ReadFromJsonAsync<T>();
-		}
+            return await response.Content.ReadFromJsonAsync<T>();
+        }
 
-		public async Task InvokePut<T>(string uri, T obj)
-		{
-			var response = await _httpClient.PutAsJsonAsync(GetUrl(uri), obj);
-			response.EnsureSuccessStatusCode();
-		}
+        public async Task InvokePut<T>(string uri, T obj)
+        {
+            var response = await httpClient.PutAsJsonAsync(GetUrl(uri), obj);
+            response.EnsureSuccessStatusCode();
+        }
 
-		public async Task InvokeDelete<T>(string uri)
-		{
-			var response = await _httpClient.DeleteAsync(GetUrl(uri));
-			response.EnsureSuccessStatusCode();
-		}
+        public async Task InvokeDelete(string uri)
+        {
+            var response = await httpClient.DeleteAsync(GetUrl(uri));
+            response.EnsureSuccessStatusCode();
+        }
 
-		private string GetUrl(string uri)
-		{
-			return $"{_baseUrl}/{uri}";
-		}
-	}
+        private string GetUrl(string uri)
+        {
+            return $"{baseUrl}/{uri}";
+        }
+    }
 }
